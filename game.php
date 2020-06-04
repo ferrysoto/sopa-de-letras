a3489a259adfd43369ab058cf6112cdaa10930f0
Ferran Herrero <ferran.herrerosoto@gmail.com>
2020-06-04 17:36:36 +0200

    letters add

--- game.php
+++ game.php
@@ -6,4 +6,26 @@
   $_SESSION['columns'] = $_POST['columns'];
   $_SESSION['words'] = $_POST['words'];
 
+  function createTable($rows, $columns) {
+
+    echo "<table id='table'>\n";
+    for ($i=0; $i <= ($rows) ; $i++) {
+      echo "<tr>\n";
+      for ($j=0; $j < $columns; $j++) {
+        $letter = chr(rand(65,90));
+        echo "<td style='border: 1px solid black; padding: 5px;'>" . $letter . "</td>";
+      }
+    }
+  }
+
+
+  createTable($_SESSION['rows'], $_SESSION['columns'])
+
+
+
+  // explode php -> añadir elemento en lista segun separa
+
+  // al clickar en una letra eliminar el boton
+  // preg_replace
+
  ?>
