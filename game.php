7815608f9eea1dfbc6367fa41416d434ce68e97d
Ferran Herrero <ferran.herrerosoto@gmail.com>
2020-06-09 19:31:38 +0200

    fix table words position

--- game.php
+++ game.php
@@ -1,98 +1,133 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Game</title>
     <link rel="stylesheet" href="style.css">
-    <title>Sopa de lletres</title>
-  </head>
+</head>
+<?php
+session_start();
 
-  <body>
-    <div class="container">
-      <?php
-      session_start();
-      echo "<h3>Está jugando:  " . $_SESSION["name"] . "</h3>";
+  function newGame($rows,$columns,$numWords) {
+    $arrayGame = createArrayFilledRandomly($rows,$columns);
+    $words = getWords($numWords);
+    $_SESSION["maxLetters"] = cntLetters($words);
 
-      $_SESSION["name"] =  $_POST['name'];
-      $_SESSION["rows"] =  $_POST['rows'];
-      $_SESSION["cols"] =  $_POST['columns'];
-      $_SESSION["words"] =  $_POST['words'];
-      $_SESSION["hits"] =  0;
-      $_SESSION["game"] = createTable($_SESSION["rows"], $_SESSION["cols"], $_SESSION["words"] );
-
-
-      function createTable($rows, $columns, $userwords) {
-            $arrayGame = createArrayFillRandom($rows, $columns);
-            $words = getWords($userwords);
-            $_SESSION['cant_letters'] = countLetters($words);
-
-            for ($indexWord = 0; $indexWord < count($words); $indexWord++) {
-            $vertical = rand(0, 1);
-            if ($vertical) {
-                $fila = rand(0, $rows - strlen($words[$indexWord])-1);
-                $columna = rand(0, $columns-1);
-                for ($i = 0; $i < strlen($words[$indexWord]); $i++) {
-                    $letra = substr($words[$indexWord], $i, 1);
-                    $nextFila = $fila + $i;
-                    $arrayGame[$nextFila][$columna] = "<td class><button type='submit' name='cell' value=\"$nextFila-$columna\">" . $letra . "</button></td>";
-                }
-            } else {
-                $fila = rand(0, $rows-1);
-                $columna = rand(0, $columns - strlen($words[$indexWord]));
-                for ($i = 0; $i < strlen($words[$indexWord]); $i++) {
-                    $letra = substr($words[$indexWord], $i, 1);
-                    $nextCol = $columna + $i;
-                    $arrayGame[$fila][$nextCol] = "<td class><button type='submit' name='cell' value=\"$fila-$nextCol\">" . $letra . "</button></td>";
-                }
-              }
-          }
-          return $arrayGame;
+    for ($indexWord = 0; $indexWord < count($words); $indexWord++) {
+      $vertical = rand(0, 1);
+      if ($vertical) {
+        $fila = rand(0, $rows - strlen($words[$indexWord])-1);
+        $columna = rand(0, $columns-1);
+        for ($i = 0; $i < strlen($words[$indexWord]); $i++) {
+          $letra = substr($words[$indexWord], $i, 1);
+          $nextFila = $fila + $i;
+          $arrayGame[$nextFila][$columna] = "<td class><button type='submit' name='cell' value=\"$nextFila-$columna\">" . $letra . "</button></td>";
         }
-
-        function CountLetters($words) {
-          $numLetters = 0;
-
-          foreach ($words as $word) {
-            $numLetters += strlen($word);
-          }
-
-          return $numLetters;
+      } else {
+        $fila = rand(0, $rows-1);
+        $columna = rand(0, $columns - strlen($words[$indexWord]));
+        for ($i = 0; $i < strlen($words[$indexWord]); $i++) {
+          $letra = substr($words[$indexWord], $i, 1);
+          $nextCol = $columna + $i;
+          $arrayGame[$fila][$nextCol] = "<td class><button type='submit' name='cell' value=\"$fila-$nextCol\">" . $letra . "</button></td>";
         }
+      }
+    }
+    return $arrayGame;
+  }
 
+  function createArrayFilledRandomly($rows, $columns) {
+    $array = [];
+    for ($i = 0; $i < $rows; $i++) {
+      for ($j = 0; $j < $columns; $j++) {
+        $randChar = chr(rand(65, 90));
+        $array[$i][$j] = "<td><button type='submit' name='cell'>$randChar</button></td>";
+      }
+    }
+    return $array;
+  }
 
-        function getWords($usernumber) {
-          $filecontents = file_get_contents('words.txt');
-          $filewords = preg_split('/[\s]+/', $filecontents, -1, PREG_SPLIT_NO_EMPTY);
-          $words = [];
+  function getWords($number) {
+     $filecontents = file_get_contents('words.txt');
+     $filewords = preg_split('/[\s]+/', $filecontents, -1, PREG_SPLIT_NO_EMPTY);
+     $words = [];
 
-          for ($i=0; $i < $usernumber; $i++) {
-            $randnumber = array_rand($filewords, 1);
-            array_push($words, $filewords[$randnumber]);
-            unset($filewords[$randnumber]);
-          }
+     for ($i=0; $i < $number; $i++) {
+          $randnumber = array_rand($filewords, 1);
+          array_push($words, $filewords[$randnumber]);
+          unset($filewords[$randnumber]);
+      }
 
-          return $words;
+    $_SESSION['palabras'] = $words;
+    return $words;
+  }
+
+  function cntLetters($arrayWords) {
+    $letters = 0;
+    foreach ($arrayWords as  $word) {
+      $letters += strlen($word);
+    }
+    return $letters;
+  }
+
+  function rmvBtn($htmlString) {
+    $htmlString = preg_replace('/<button [^>]*>/', "", $htmlString);
+    $htmlString = preg_replace('/<\/button>/', "", $htmlString);
+    return $htmlString;
+  }
+
+  function ifCorrect($box) {
+    $i = explode("-", $box)[0];
+    $j = explode("-", $box)[1];
+
+    $aux = preg_replace('/class/',"class='success'", $_SESSION["game"][$i][$j]);
+    $aux = rmvBtn($aux);
+    $_SESSION["game"][$i][$j] = $aux;
+  }
+
+  function render($array) {
+    foreach ($array as $row) {
+      echo "<tr>";
+        foreach ($row as $value) {
+          echo $value;
         }
+        echo "</tr>";
+      }
+    }
 
-        function createArrayFillRandom($rows, $columns) {
-            $array = [];
-            for ($i = 0; $i < $rows; $i++) {
-                for ($j = 0; $j < $columns; $j++) {
-                    $randChar = chr(rand(65, 90));
-                    $array[$i][$j] = "<td><button type='submit' name='cell'> $randChar </button></td>";
-                }
-            }
-            return $array;
-        }
+  if (!isset($_SESSION["game"]) or is_null($_SESSION["game"])){
 
-        function removeButton($htmlString) {
-            $htmlString = preg_replace('/<button [^>]*>/',"",$htmlString);
-            $htmlString = preg_replace('/<\/button>/',"",$htmlString);      
-            return $htmlString;
-        }
+    if (!isset($_POST["username"],$_POST["rows"], $_POST["columns"], $_POST["words"])){
+      header('Location:./index.php');
+      die();
+    }
+    $_SESSION["username"] =  $_POST["username"];
+    $_SESSION["rows"] =  $_POST["rows"];
+    $_SESSION["columns"] =  $_POST["columns"];
+    $_SESSION["words"] =  $_POST["words"];
+    $_SESSION["tries"] =  0;
+    $_SESSION["game"] = newGame($_SESSION["rows"], $_SESSION["columns"], $_SESSION["words"]);
 
+  } else if (isset($_GET["cell"])) {
+    if (empty($_GET["cell"])) {
+      $_SESSION["tries"]++;
+    }else{
+      ifCorrect($_GET["cell"]);
+    }
+  }
+  $arrayGame = $_SESSION["game"];
 
-        ?>
-    </div>
-    <a class="btn" href="/index.php">Inicio</a>
-  </body>
+  ?>
+    <body>
+      <div class="container">
+        <h3>Está Jugando:  <?php print_r($_SESSION['username']); ?></h3>
+        <br>
+        Palabras <?php print_r($_SESSION['palabras']); ?>
+        <form action="./game.php" method="get">
+          <table><?php render($arrayGame) ?></table>
+        </form>
+        <form action="./index.php" method="get">
+          <button class="btn" type="submit">Menu</button>
+        </form>
+      </div>
+    </body>
 </html>
--- index.php
+++ index.php
@@ -1,29 +1,25 @@
 <?php
-  session_start();
-  session_destroy();
- ?>
-
+        session_start();
+        session_destroy();
+?>
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Sopa de letras</title>
-  </head>
-  <body>
-    <h1>Sopa de letras</h1>
-    <form action="game.php" method="post">
-      <label for="name">Nombre del jugador: </label>
-      <input type="text" name="name" value="" required>
-      <br>
-      <label for="rows">Filas: </label>
-      <input type="number" name="rows" value="" min="10" max="20" required>
-      <br>
-      <label for="columns">Columnas: </label>
-      <input type="number" name="columns" value="" min="10" max="20" required>
-      <br>
-      <label for="words">Palabras: </label>
-      <input type="number" name="words" value="" min="2" max="10" required>
-      <button type="submit" name="button">¡Jugar!</button>
+<html lang="en">
+
+<head>
+    <title>Menu</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+  <div class="container">
+    <form action="./game.php" method='post'>
+      Nombre: <input type="text" name="username" required minlength="2" maxlength="14"><br>
+      Filas: <input type="number" name="rows" required min="10" max="20"><br>
+      Columnas:<input type="number" name="columns" required min="10" max="20"><br>
+      Cantidad de palabras :<input type="number" name="words"  min="2" max="12"><br>
+      <input class="btn" type="submit">
     </form>
-  </body>
+  </div>
+</body>
+
 </html>
